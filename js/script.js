8d98b0085c4d8e061bcc0b40eafd2da4ac71f5ae
carpophorus <markomrkonjic9@gmail.com>
2020-03-25 17:23:21 +0100

    switch back to localhost

--- js/script.js
+++ js/script.js
@@ -6,8 +6,9 @@
   RGZ.loginInfo = '';
 
   //DBS TEST
-  RGZ.apiRoot = 'http://10.0.1.251:8083/api/';
-  //RGZ.apiRoot = 'http://localhost:50398/api/';
+  //RGZ.apiRoot = 'http://10.0.13.9:8083/api/';
+  //RGZ.apiRoot = 'http://10.0.1.251:8083/api/';
+  RGZ.apiRoot = 'http://localhost:50398/api/';
 
   //RGZ LIVE
   //RGZ.apiRoot = 'http://93.87.56.76:8083/api/';
